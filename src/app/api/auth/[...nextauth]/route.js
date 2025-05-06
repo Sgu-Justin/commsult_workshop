2f7928cd302cabb26a9c9bd1cbfb20c0085b9750
Sgu-Justin <justin.sye@student.sgu.sc.id>
2025-05-06 14:29:07 +0700

    first commit

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,29 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+const authHandler = NextAuth({
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                email: { label: "email", type: "text" },
+                password: { label: "password", type: "password" },
+            },
+            async authorize(credentials) {
+                try {
+                    const { email,password } = credentials;
+                    if(email === "demo@example.com" && password === "password"){
+                        return { id: 1, name: "John Doe"};
+                    }
+                }   catch (e) {
+                    return null
+                }
+            }
+        })
+    ],
+    pages: {
+       signIn: '/login',
+    }
+})
+
+export { authHandler as GET, authHandler as POST };
